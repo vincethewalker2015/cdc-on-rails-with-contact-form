1226ccb04964452510403e775081010940822c1d
vincentbwalker <vincethewalker@gmail.com>
2019-01-09 14:29:57 +0000

    projects and details controllers and created project form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
     
     root 'pages#home'
+    
+    resources :projects
+    resources :details
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
