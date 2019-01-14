de0d5239d78027f0d694faba503aa68e03e67034
vincentbwalker <vincethewalker@gmail.com>
2019-01-14 13:19:50 +0000

    Added JS to change nav-bar color when scrolling in a light color area

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+// Navbar Color Toggle when scrolling over a lighter page
+
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
