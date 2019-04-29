5ac049111c985f0cd5297a040366d1a87dfda1a1
vincentbwalker <vincethewalker@gmail.com>
2019-04-29 15:20:29 +0100

    Added 'simple_format' for paragraph breaks in show pages

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,7 +2,7 @@ class Detail < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
     
-    belongs_to :user
+    #belongs_to :user
     
     mount_uploader :picture, PictureUploader
     
