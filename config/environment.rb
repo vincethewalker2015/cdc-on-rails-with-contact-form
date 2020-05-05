6c951588af68ad985181cb25d9019affa88ab350
Vincent <vincethewalker@yahoo.co.uk>
2020-05-05 19:44:46 +0100

    Final mailer set up for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,17 @@
 # Load the Rails application.
 require_relative 'application'
+require 'carrierwave/orm/activerecord'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-require 'carrierwave/orm/activerecord'
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
+  }
+
